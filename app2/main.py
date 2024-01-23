4f71cd9c53a0586055cf5e29b5441971329532c9
Shital Kandel <kandelshittal@gmail.com>
2024-01-23 13:22:41 +0545

    Added: hash password

--- app2/main.py
+++ app2/main.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect, session
 import psycopg2
-import bcrypt
-
+from passlib.hash import pbkdf2_sha256
 
 app = Flask(__name__)
 app.secret_key = 'in.@123@'
-
 
 conn = psycopg2.connect(database="flask_app", user="shital", password='123456', host="localhost", port="5432")
 cursor = conn.cursor()
@@ -13,8 +11,8 @@ cursor = conn.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS flask_app (
         id SERIAL PRIMARY KEY,
-        username VARCHAR(100),
-        password VARCHAR(100)
+        username VARCHAR,
+        password VARCHAR
     );
 ''')
 conn.commit()
@@ -25,24 +23,28 @@ conn.commit()
 def home():
     return render_template('home.html')
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
-        cursor.execute("SELECT * FROM flask_app WHERE username = %s;", (username,))
+        password_hash = pbkdf2_sha256.hash(password)
+
+        cursor.execute("SELECT * FROM flask_app WHERE (username, password) = (%s,%s);", (username,password_hash))
         existing_user = cursor.fetchone()
 
-        if existing_user:
+        if existing_user and pbkdf2_sha256.verify(password, existing_user[2]):
             return render_template('register.html', error='Username already exists.')
 
         cursor.execute("INSERT INTO flask_app (username, password) VALUES (%s, %s);", (username, password))
         conn.commit()
 
-        return redirect(url_for('login'))
+        return redirect(url_for('login')) 
 
     return render_template('register.html')
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -50,17 +52,15 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
-        cursor.execute("SELECT * FROM flask_app WHERE username = %s AND password = %s;", (username,
-                                                                                          password))
+        
+        hash_password = pbkdf2_sha256.verify(password,user['username'])
+        
+        cursor.execute("SELECT * FROM flask_app WHERE (username,password) = (%s,%s);", (username,hash_password))
         user = cursor.fetchone()
 
-        if user:
+        if user and pbkdf2_sha256.verify(password, user['username']):
             session['username'] = username
-            if user and not bcrypt.hashpw(user[2], password):
-                reset = reset_password()
-                return reset ,redirect(url_for('reset_password'))
-
+            return redirect(url_for('dashboard'))
         return "Please check your Username and Password"
     return render_template('login.html')
 
@@ -69,29 +69,28 @@ def login():
 def reset_password():
     if request.method == 'POST':
         username = request.form['username']
-        old_password = request.form['old_password']
         new_password = request.form['new_password']
 
         cursor.execute("SELECT * FROM flask_app WHERE username = %s;", (username,))
         user = cursor.fetchone()
 
-        if user and bcrypt.hashpw(user[2], old_password):
-            hashed_password = bcrypt.checkpw(new_password).decode('utf-8')
+        if user and pbkdf2_sha256.hashpw(new_password.encode('utf-8'), user[2].encode('utf-8')):
 
-            cursor.execute("UPDATE flask_app SET password = %s WHERE username = %s;", (hashed_password, username))
+            cursor.execute("UPDATE flask_app SET password = %s WHERE username = %s;", ( username))
             conn.commit()
 
             return redirect(url_for('login'))
         else:
-            return render_template('reset_password.html', error='Invalid username or old password.')
+            return render_template('reset_password.html', error='Invalid username or password.')
 
     return render_template('reset_password.html')
+
 
 
 @app.route('/dashboard')
 def dashboard():
     if 'username' in session:
-        return render_template('dashboard.html')
+        return render_template('dashboard.html', username=session['username'])
     else:
         return redirect(url_for('login'))
     
@@ -99,9 +98,8 @@ def dashboard():
 
 @app.route('/logout')
 def logout():
+    session.pop('username', None)
     return redirect(url_for('home'))
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
